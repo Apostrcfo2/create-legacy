00004a18775c8abc50a27a805a3e1c59e127b29b
Siepert123 <createlegacy69@gmail.com>
2025-05-12 14:56:14 +0200

    Sideways large cogwheel

--- src/main/java/nl/melonstudios/create/tileentity/TileEntityCogwheel.java
+++ src/main/java/nl/melonstudios/create/tileentity/TileEntityCogwheel.java
@@ -1,10 +1,16 @@
 package nl.melonstudios.create.tileentity;
 
 import com.melonstudios.melonlib.misc.BlockStateProperties;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import nl.melonstudios.create.util.Utils;
 import nl.melonstudios.create.util.interfaces.ICogwheel;
+import nl.melonstudios.create.util.interfaces.IRotate;
+
+import java.util.LinkedList;
 
 public class TileEntityCogwheel extends TileEntityKinetic {
     public boolean isLarge() {
@@ -14,5 +20,21 @@ public class TileEntityCogwheel extends TileEntityKinetic {
     @SideOnly(Side.CLIENT)
     public EnumFacing.Axis getRenderAxis() {
         return this.getState().getValue(BlockStateProperties.AXIS);
+    }
+
+    @Override
+    public LinkedList<BlockPos> addPropagationLocations(IRotate block, IBlockState state, LinkedList<BlockPos> neighbours) {
+        super.addPropagationLocations(block, state, neighbours);
+
+        if (!this.isLarge()) return neighbours;
+
+        EnumFacing.Axis axis = this.getState().getValue(BlockStateProperties.AXIS);
+
+        BlockPos pos1 = this.getPos().offset(EnumFacing.getFacingFromAxis(EnumFacing.AxisDirection.POSITIVE, axis));
+        for (EnumFacing facing : Utils.getSurrounding(axis)) neighbours.add(pos1.offset(facing));
+        BlockPos pos2 = this.getPos().offset(EnumFacing.getFacingFromAxis(EnumFacing.AxisDirection.NEGATIVE, axis));
+        for (EnumFacing facing : Utils.getSurrounding(axis)) neighbours.add(pos2.offset(facing));
+
+        return neighbours;
     }
 }
--- src/main/java/nl/melonstudios/create/util/Utils.java
+++ src/main/java/nl/melonstudios/create/util/Utils.java
@@ -1,9 +1,12 @@
 package nl.melonstudios.create.util;
 
+import com.google.common.collect.ImmutableList;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
 
 @SuppressWarnings("all")
 public class Utils {
@@ -22,5 +25,20 @@ public class Utils {
     }
     public static int dist_manh(BlockPos a, BlockPos b) {
         return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY()) + Math.abs(a.getZ() - b.getZ());
+    }
+
+    public static final List<EnumFacing> SURROUND_X =
+            ImmutableList.of(EnumFacing.UP, EnumFacing.DOWN, EnumFacing.NORTH, EnumFacing.SOUTH);
+    public static final List<EnumFacing> SURROUND_Y =
+            ImmutableList.of(EnumFacing.NORTH, EnumFacing.SOUTH, EnumFacing.EAST, EnumFacing.WEST);
+    public static final List<EnumFacing> SURROUND_Z =
+            ImmutableList.of(EnumFacing.UP, EnumFacing.DOWN, EnumFacing.EAST, EnumFacing.WEST);
+    public static List<EnumFacing> getSurrounding(EnumFacing.Axis axis) {
+        switch (axis) {
+            case X: return SURROUND_X;
+            case Y: return SURROUND_Y;
+            case Z: return SURROUND_Z;
+            default:return Collections.emptyList();
+        }
     }
 }
